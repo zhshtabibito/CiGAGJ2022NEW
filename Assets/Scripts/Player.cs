74b0f5da070780ea5be6829fb7451224a14c1924
zhshtabibito <zhshtabibito@foxmail.com>
2022-06-25 11:04:12 +0800

    base block and char

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float spd = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector2.left * Time.deltaTime * spd);
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * Time.deltaTime * spd);
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector2.up * Time.deltaTime * spd);
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector2.down * Time.deltaTime * spd);
+            transform.localScale = new Vector3(1, -1, 1);
+        }
+    }
+}
