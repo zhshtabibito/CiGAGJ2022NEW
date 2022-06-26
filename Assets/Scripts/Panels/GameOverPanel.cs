6d3103f5ae996149117fc7d346f749ea29b234cf
zhshtabibito <zhshtabibito@foxmail.com>
2022-06-26 14:05:30 +0800

    fix gameover

--- Assets/Scripts/Panels/GameOverPanel.cs
+++ Assets/Scripts/Panels/GameOverPanel.cs
@@ -1,14 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class GameOverPanel : BasePanel
 {
-    static readonly string Path = "Prefabs/Panels/StartPanel";
+    static readonly string Path = "Prefabs/Panels/GameOverPanel";
     public GameOverPanel() : base(new PanelInfo(Path))
     {
 
     }
 
- 
+    protected override void InitEvent()
+    {
+        Debug.Log(ActivePanel.GetOrAddComponentInChildren<Button>("Button"));
+        ActivePanel.GetOrAddComponentInChildren<Button>("Button").onClick.AddListener(() =>
+        {
+            OnRetry();
+        });
+    }
+    private void OnRetry()
+    {
+        SceneManager.LoadScene($"Level{GameObject.Find("LevelRoot").GetComponent<LevelInfo>().LevelID}");
+        PanelManager.Instance.Pop();
+        PanelManager.Instance.Push(new LevelPanel());
+    }
+
 }
